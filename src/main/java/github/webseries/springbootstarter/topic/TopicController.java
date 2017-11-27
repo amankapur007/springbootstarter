d44781a5e837c6f2e593da9c30ec34e68e49c3ce
Aman <akapur07@gmail.com>
2017-11-27 16:30:59 +0530

    Removed content of Unit -2

--- src/main/java/github/webseries/springbootstarter/topic/TopicController.java
+++ src/main/java/github/webseries/springbootstarter/topic/TopicController.java
@@ -1,50 +1,18 @@
 package github.webseries.springbootstarter.topic;
 
+import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 
-	@Autowired
-	private TopicService topicService;
-
-	public TopicService getTopicService() {
-		return topicService;
-	}
-
-	public void setTopicService(TopicService topicService) {
-		this.topicService = topicService;
-	}
-
 	@RequestMapping(value = "/topics")
 	public List<Topic> getAllTopic() {
-		return topicService.getAllTopics();
-	}
-	
-	@RequestMapping(value="/topics/{id}")
-	public Topic getTopic(@PathVariable String id){
-		return topicService.getTopic(id);
-	}
-	
-	@RequestMapping(value="/topics",method=RequestMethod.POST)
-	public void addTopic(@RequestBody Topic topic){
-		topicService.addTopic(topic);
-	}
-	
-	@RequestMapping(value="/topics/{id}",method=RequestMethod.PUT)
-	public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
-		topicService.updateTopic(topic, id);
-	}
-	
-	@RequestMapping(value="/topics/{id}",method=RequestMethod.DELETE)
-	public void updateTopic(@PathVariable String id){
-		topicService.deleteTopic(id);
+		return Arrays.asList(new Topic("spring", "Spring Framework", "The tutorial for spring framework"),
+				new Topic("java", "Core Java", "The tutorial for core java"),
+				new Topic("javascript", "Javascript", "The tutorial for javascript"));
 	}
 }
